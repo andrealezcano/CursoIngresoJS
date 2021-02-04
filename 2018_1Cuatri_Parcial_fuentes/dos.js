6037984d1b89b7218e3ddafb3e757c6126b33ca9
andrealezcano <78226534+andrealezcano@users.noreply.github.com>
2021-02-03 21:56:57 -0300

    Add files via upload

--- 2018_1Cuatri_Parcial_fuentes/dos.js
+++ 2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+
+    var nombreIngresado;
+
+    var localidadIngresado;
+
+    var mensaje;
+
+    nombreIngresado = elNombre.value;
+
+    localidadIngresado = laLocalidad.value;
+
+
+  mensaje = nombreIngresado + localidadIngresado;
+
+  mensaje = alert("Usted es" + nombreIngresado + "y vive en la localidad de" + localidadIngresado);
   
+
 }
--- 2018_1Cuatri_Parcial_fuentes/tres.js
+++ 2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,29 @@
 function mostrar()
 {
 
+    var precio ;
+    
+    var porcentajeDescuento ;
+
+    var precioFinal ;
+
+    var precioParseado ;
+
+    var porcentajeParseado ;
+
+    precio = prompt("Ingrese el precio") ;
+
+    porcentajeDescuento = prompt("Ingrese el descuento") ;
+
+    precioParseado = parseInt(precio) ;
+
+    porcentajeParseado = parseInt(porcentajeParseado) ;
+
+    porcentajeDescuento = porcentajeDescuento/100 ;
+
+
+    precioFinal = precio * (porcentajeDescuento) ;
+
+     elPrecioFinal.value =  precioFinal ;
+
 }
